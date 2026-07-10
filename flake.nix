479f81007d68ba30a27dce1c1c185ffa4833e6a4
matthis-k <matthis.kaelble@gmail.com>
2026-07-10 21:57:44 +0200

    minimal start.

--- flake.nix
+++ flake.nix
@@ -1,39 +1,38 @@
 {
-  description = "Phenix agent harness wrappers for Pi";
+  description = "Phenix agent harness for Pi";
 
-  inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    phenix-pins.url = "github:matthis-k/phenix-pins";
+  outputs = { nixpkgs, self }: let
+    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+  in {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs { inherit system; };
 
-    nixpkgs.follows = "phenix-pins/nixpkgs";
-    nixpkgs-unstable.follows = "phenix-pins/nixpkgs-unstable";
-
-    nix-wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
-    nix-wrapper-modules.inputs.nixpkgs.follows = "phenix-pins/nixpkgs";
-
-    phenix-tend.url = "github:matthis-k/phenix-tend";
-    phenix-tend.inputs.phenix-pins.follows = "phenix-pins";
-
-    phenix-stitch.url = "github:matthis-k/phenix-stitch";
-    phenix-stitch.inputs.phenix-pins.follows = "phenix-pins";
-  };
-
-  outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-
-      imports = [ ./modules/package.nix ];
-
-      flake = {
-        flakeModules.default = import ./modules/flake-module.nix;
-
-        wrappers.piModule =
-          (inputs.nix-wrapper-modules.lib.evalModule (import ./modules/wrapper-module.nix)).config;
+      phenixConfig = pkgs.stdenv.mkDerivation {
+        name = "phenix-pi-config";
+        src = ./config/phenix-pi;
+        installPhase = ''
+          mkdir -p $out
+          cp -R . $out/
+        '';
       };
-    };
+
+      wrappedPi = pkgs.symlinkJoin {
+        name = "pi";
+        paths = [ pkgs.pi-coding-agent ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/pi \
+            --set PI_PACKAGE_DIR "${phenixConfig}" \
+            --set PI_SKIP_VERSION_CHECK "1" \
+            --set PI_TELEMETRY "0"
+        '';
+      };
+    in {
+      default = wrappedPi;
+      pi = wrappedPi;
+      phenix-config = phenixConfig;
+    });
+  };
 }
