3497087db23e60b76de1240d59acfa631b883d80
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-13 18:48:08 +0200

    feat(module): export wrapped Stitch packages

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -8,6 +8,8 @@
         pi = inputs.self.packages.${system}.pi;
         piPackage = inputs.self.packages.${system}.phenix-pi-package;
         tend = inputs.self.packages.${system}.tend;
+        stitch = inputs.self.packages.${system}.stitch;
+        stitchMcp = inputs.self.packages.${system}.stitch-mcp;
       };
     };
 }
