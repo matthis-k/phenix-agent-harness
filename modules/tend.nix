c8bddba45c99e0a20624816af2faf93e7e411055
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-13 11:29:12 +0200

    style: apply nixfmt to Tend module

--- modules/tend.nix
+++ modules/tend.nix
@@ -31,46 +31,52 @@
 
       source = lib.cleanSource ../.;
 
-      tendProfileValidation = pkgs.runCommand "phenix-tend-profile-validation" {
-        nativeBuildInputs = [
-          tendCli
-          pkgs.git
-        ];
-        inherit source;
-      } ''
-        cp -rT "$source" source
-        chmod -R u+w source
-        cd source
+      tendProfileValidation =
+        pkgs.runCommand "phenix-tend-profile-validation"
+          {
+            nativeBuildInputs = [
+              tendCli
+              pkgs.git
+            ];
+            inherit source;
+          }
+          ''
+            cp -rT "$source" source
+            chmod -R u+w source
+            cd source
 
-        git init --quiet
-        git add -A
-        tend validate --profiles
+            git init --quiet
+            git add -A
+            tend validate --profiles
 
-        touch "$out"
-      '';
+            touch "$out"
+          '';
 
-      tendNixCheck = pkgs.runCommand "phenix-tend-nix-check" {
-        nativeBuildInputs = tendRuntimeInputs;
-        inherit source;
-      } ''
-        cp -rT "$source" source
-        chmod -R u+w source
+      tendNixCheck =
+        pkgs.runCommand "phenix-tend-nix-check"
+          {
+            nativeBuildInputs = tendRuntimeInputs;
+            inherit source;
+          }
+          ''
+            cp -rT "$source" source
+            chmod -R u+w source
 
-        rm -rf source/modules/phenix-pi/node_modules
-        ln -s ${phenixPiPackage}/node_modules source/modules/phenix-pi/node_modules
+            rm -rf source/modules/phenix-pi/node_modules
+            ln -s ${phenixPiPackage}/node_modules source/modules/phenix-pi/node_modules
 
-        cd source
-        git init --quiet
-        git add -A
+            cd source
+            git init --quiet
+            git add -A
 
-        export HOME="$TMPDIR/home"
-        export PHENIX_PI_ROOT="$PWD/modules/phenix-pi"
-        mkdir -p "$HOME"
+            export HOME="$TMPDIR/home"
+            export PHENIX_PI_ROOT="$PWD/modules/phenix-pi"
+            mkdir -p "$HOME"
 
-        tend run --mode full --phase verify --profile nix-check
+            tend run --mode full --phase verify --profile nix-check
 
-        touch "$out"
-      '';
+            touch "$out"
+          '';
     in
     {
       packages = {
