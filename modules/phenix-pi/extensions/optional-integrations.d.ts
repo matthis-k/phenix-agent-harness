d3c617560bba821232c96310308791b0dc0ede82
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-15 04:24:50 +0200

    build(pi): declare Hypa redirect helpers

--- modules/phenix-pi/extensions/optional-integrations.d.ts
+++ modules/phenix-pi/extensions/optional-integrations.d.ts
@@ -1,10 +1,19 @@
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 
-type ExtensionRegistrar = (pi: ExtensionAPI) => void | Promise<void>;
+ type ExtensionRegistrar = (pi: ExtensionAPI) => void | Promise<void>;
 
 declare module "@hypabolic/pi-hypa/extensions/index.ts" {
   const register: ExtensionRegistrar;
   export default register;
+}
+
+declare module "@hypabolic/pi-hypa/extensions/rewrite-client.ts" {
+  export function getExecArgs(binary: string, args: string[]): [string, string[]];
+  export function resolveHypaBinary(binary: string): string;
+}
+
+declare module "@hypabolic/pi-hypa/extensions/tools.ts" {
+  export function buildReadCommand(path: string, offset?: number, limit?: number): string;
 }
 
 declare module "pi-lsp/extensions/pi-lsp/index.ts" {
