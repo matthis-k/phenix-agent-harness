08637ab28e09adfe8248da8304658c2aea8c6cba
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-22 08:59:39 +0200

    feat(authority): expose unified authority facade

---
+++ modules/phenix-pi/packages/phenix-suite/authority/index.ts
@@ -0,0 +1,33 @@
+export { assurancePolicyFor } from "./assurance.ts";
+export type { AssurancePolicy, AssurancePolicyInput } from "./assurance.ts";
+export { createExecutionAuthority, ExecutionAuthority } from "./service.ts";
+export type { ExecutionAuthorityOptions, TaskProjectionInput } from "./service.ts";
+export {
+  emptyAuthorityPersistence,
+  FileExecutionAuthorityStore,
+  InMemoryExecutionAuthorityStore,
+} from "./store.ts";
+export type { ExecutionAuthorityStore } from "./store.ts";
+export type {
+  AcceptanceDecision,
+  AssuranceLevel,
+  AuthorityMutation,
+  BeginObjectiveInput,
+  CreateNodeInput,
+  DynamicNodeRequest,
+  ExecutionAuthorityEvent,
+  ExecutionAuthorityEventType,
+  ExecutionAuthorityPersistence,
+  ExecutionAuthoritySnapshot,
+  ExecutionHandleRecord,
+  ExecutionMode,
+  ExecutionNodeRecord,
+  HandleAcceptanceState,
+  HandleRuntimeState,
+  LegalAction,
+  LegalActionKind,
+  ObjectiveRecord,
+  ObjectiveState,
+  RegisterHandleInput,
+  RuntimeHandleUpdate,
+} from "./types.ts";
