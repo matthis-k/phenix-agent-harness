e2e8955f570b94c95f19b6b4c4854225097c8a8f
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-25 04:13:13 +0200

    refactor: remove TypeScript workflow manifest

--- modules/phenix-pi/definitions/workflows/index.ts
+++ modules/phenix-pi/definitions/workflows/index.ts
@@ -3,7 +3,3 @@ export {
   qaWorkflow,
   workflowDefinitions,
 } from "../../composition/bundled-definitions.ts";
-export {
-  BUNDLED_WORKFLOW_SOURCE_NAMES,
-  type BundledWorkflowSourceName,
-} from "./manifest.ts";
