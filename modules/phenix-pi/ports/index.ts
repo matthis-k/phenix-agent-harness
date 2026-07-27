6fda8ca17913eef4575192b0ad828dc58258b808
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-27 02:50:36 +0200

    Export integration status port

--- modules/phenix-pi/ports/index.ts
+++ modules/phenix-pi/ports/index.ts
@@ -1,5 +1,6 @@
 export * from "./agent-session-backend.ts";
 export * from "./clock.ts";
+export * from "./integration-status.ts";
 export * from "./local-operation-runner.ts";
 export * from "./model-resolver.ts";
 export * from "./run-ledger.ts";
