0b5081d74545bd7a9dfa25c07221193241044b04
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-29 22:14:53 +0200

    Update theme registration contract for single entrypoint

--- modules/phenix-pi/tests/phenix-theme.test.ts
+++ modules/phenix-pi/tests/phenix-theme.test.ts
@@ -78,10 +78,15 @@ interface PackageDocument {
 
 const THEME = readJson<ThemeDocument>("../themes/catppuccin-mocha.json");
 const PACKAGE = readJson<PackageDocument>("../package.json");
+const PHENIX_ENTRYPOINT = readFileSync(new URL("../extension/phenix.ts", import.meta.url), "utf8");
 
-test("registers the Mocha theme and activates it before the other Phenix extensions", () => {
+test("registers the Mocha theme through the single Phenix extension", () => {
   assert.deepEqual(PACKAGE.pi.themes, ["./themes/catppuccin-mocha.json"]);
-  assert.equal(PACKAGE.pi.extensions[0], "./extension/theme-extension.ts");
+  assert.deepEqual(PACKAGE.pi.extensions, ["./extension/phenix.ts"]);
+  assert.ok(
+    PHENIX_ENTRYPOINT.indexOf("registerTheme(pi)") <
+      PHENIX_ENTRYPOINT.indexOf("registerRuntime(pi)"),
+  );
   assert.equal(THEME.name, PHENIX_THEME_NAME);
 });
 
