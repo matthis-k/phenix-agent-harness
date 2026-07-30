753121b303b773e152bed25cba90001fd38db76d
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-30 10:17:45 +0200

    Route workspace built-ins without extension command conflicts (#215)
    * Add command action context to Pi extension API
    
    * Route workspace built-ins without command registration
    
    * Test workspace built-in routing without registration
    
    * Stop registering shadow Pi built-ins
    
    * Apply Pi command action patch
    
    * Apply workspace builtin test formatting

--- modules/phenix-pi/extension/phenix.ts
+++ modules/phenix-pi/extension/phenix.ts
@@ -5,7 +5,7 @@ import registerResultDisplay from "./result-display.ts";
 import registerRuntime from "./root-extension.ts";
 import registerTheme from "./theme-extension.ts";
 import registerVisualizationDisplay from "./visualization-display.ts";
-import { registerWorkspaceStandardBuiltins } from "./workspace/workspace-standard-builtins.ts";
+import { withWorkspaceStandardBuiltins } from "./workspace/workspace-standard-builtin-api.ts";
 import registerWorkspaceStatus from "./workspace-status-extension.ts";
 
 /**
@@ -18,8 +18,7 @@ export default async function phenix(pi: ExtensionAPI): Promise<void> {
   registerTheme(pi);
   await registerRuntime(pi);
   registerWorkspaceStatus(pi);
-  registerWorkspaceStandardBuiltins(pi);
-  registerWorkspace(pi);
+  registerWorkspace(withWorkspaceStandardBuiltins(pi));
   registerResultDisplay(pi);
   registerVisualizationDisplay(pi);
 }
