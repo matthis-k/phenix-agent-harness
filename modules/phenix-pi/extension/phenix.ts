dc9750e9e59170a57f7b4fd886f7252f5ff833b6
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-31 02:52:49 +0200

    Register user form extension

--- modules/phenix-pi/extension/phenix.ts
+++ modules/phenix-pi/extension/phenix.ts
@@ -4,6 +4,7 @@ import registerWorkspace from "./default-workspace-extension.ts";
 import registerResultDisplay from "./result-display.ts";
 import registerRuntime from "./root-extension.ts";
 import registerTheme from "./theme-extension.ts";
+import registerUserForms from "./user-form-extension.ts";
 import registerVisualizationDisplay from "./visualization-display.ts";
 import { withWorkspaceStandardBuiltins } from "./workspace/workspace-standard-builtin-api.ts";
 import registerWorkspaceStatus from "./workspace-status-extension.ts";
@@ -18,6 +19,7 @@ export default async function phenix(pi: ExtensionAPI): Promise<void> {
   registerTheme(pi);
   await registerRuntime(pi);
   registerWorkspaceStatus(pi);
+  registerUserForms(pi);
   registerWorkspace(withWorkspaceStandardBuiltins(pi));
   registerResultDisplay(pi);
   registerVisualizationDisplay(pi);
