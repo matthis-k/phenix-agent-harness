dcd242c618660ae36b629ef7461d5579612a97a2
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-08-03 00:50:57 +0200

    Remove obsolete task terminal view

--- modules/phenix-pi/extension/workspace/views/tasks-view.ts
+++
@@ -1,9 +0,0 @@
-import { tasksWorkspaceView as semanticTasksWorkspaceView } from "../../../application/workspace/views/tasks-view.ts";
-import { withTerminalWorkspaceView } from "./workspace-view-terminal.ts";
-
-export {
-  projectWorkspaceTasks,
-  type WorkspaceTaskRow,
-} from "../../../application/workspace/views/tasks-view.ts";
-
-export const tasksWorkspaceView = withTerminalWorkspaceView(semanticTasksWorkspaceView);
