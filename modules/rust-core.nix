caf58bae09682ccadbf352fe04bb4ffc32c2419d
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-08-04 10:42:12 +0200

    build(rust): add reproducible core check

---
+++ modules/rust-core.nix
@@ -0,0 +1,33 @@
+{ ... }:
+
+{
+  perSystem =
+    { pkgs, ... }:
+    let
+      source = pkgs.lib.cleanSource ../rust;
+      check = pkgs.runCommand "phenix-rust-core-check"
+        {
+          nativeBuildInputs = [
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rustc
+            pkgs.rustfmt
+          ];
+        }
+        ''
+          cp -R ${source}/. source
+          chmod -R u+w source
+          cd source
+          export HOME="$TMPDIR/home"
+          export CARGO_HOME="$TMPDIR/cargo"
+          cargo fmt --all --check
+          cargo clippy --workspace --all-targets --locked --offline -- -D warnings
+          cargo test --workspace --locked --offline
+          touch "$out"
+        '';
+    in
+    {
+      checks.phenix-rust-core = check;
+      packages.phenix-rust-core-check = check;
+    };
+}
