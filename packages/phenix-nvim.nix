2973c71de409176f5c0d2afd6f67be40050896b7
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-08-11 04:48:46 +0200

    nvim: drop nui dependency from minimal frontend

--- packages/phenix-nvim.nix
+++ packages/phenix-nvim.nix
@@ -1,13 +1,9 @@
-{
-  vimPlugins,
-  vimUtils,
-}:
+{ vimUtils }:
 
 vimUtils.buildVimPlugin {
   pname = "phenix-nvim";
   version = "0";
   src = ../nvim;
-  dependencies = [ vimPlugins.nui-nvim ];
 
   meta.description = "Neovim frontend for the Phenix agent harness";
 }
